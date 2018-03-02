784c6541d2235697807cdbe0542b13adc2cb4aa0
lastingyeh <lastingyeh@gmail.com>
2018-03-02 17:50:14 +0800

    server db test

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -12,31 +12,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // for json data
 app.use(bodyParser.json());
 
-// app.post('/todos', (req, res) => {
-// 	const todo = new Todo({
-// 		text: req.body.text
-// 	});
+app.post('/todos', (req, res) => {
+	var todo = new Todo({
+		text: req.body.text
+	});
 
-// 	todo
-// 		.save()
-// 		.then(doc => res.send(doc))
-// 		.catch(e => res.status(400).send(e));
-// });
-
-app.post('/todos', async (req, res) => {
-	
-	try {
-        const todo = new Todo({
-            text: req.body.text
-        });
-
-		const doc = await todo.save();
-		res.send(doc);
-	} catch (error) {
-		res.status(400).send(e);
-	}
+	todo.save().then(
+		doc => {
+			res.send(doc);
+		},
+		e => {
+			res.status(400).send(e);
+		}
+	);
 });
 
 app.listen(3000, () => {
 	console.log('Started on port 3000');
 });
+
+module.exports = app;
